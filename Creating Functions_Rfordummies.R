6d4145c3e07c50349b98ba759c07671dc076693e
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-06-16 12:11:03 -0700

    2020.06.16 1208PST already did and was confused with the F to C SW carpentry tutorial moving to R for dummies

---
+++ Creating Functions_Rfordummies.R
@@ -0,0 +1,37 @@
+##Hello World
+
+#*********************************
+## Version Check
+#********************************* 
+R.version
+
+## Author: S W Carpentry
+## Title: Creating Functions
+## Date: June 2020
+
+#*********************************
+##Libraries
+#********************************* 
+
+
+
+#*********************************
+##Table of Contents
+#********************************* 
+
+# 1.0 Define a function that takes arguments.
+# 2.0 Return a value from a function.
+# 3.0 Test a function.
+# 4.0 Set default values for function arguments.
+# 5.0 Explain why we should divide programs into small, single-purpose functions.
+
+
+
+#*********************************
+##1.0 Define a function
+#********************************* 
+
+
+
+
+
