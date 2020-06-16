aaedc2ff808bc007f69bfaa62c81b9bfdcc79756
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-06-16 12:14:16 -0700

    2020.06.16 1210PST functions for dummies

--- Creating Functions_Rfordummies.R
+++ Creating Functions_Rfordummies.R
@@ -6,7 +6,7 @@
 R.version
 
 ## Author: S W Carpentry
-## Title: Creating Functions
+## Title: https://www.dummies.com/programming/r/how-to-create-a-function-in-r/
 ## Date: June 2020
 
 #*********************************
@@ -18,12 +18,20 @@ R.version
 #*********************************
 ##Table of Contents
 #********************************* 
-
-# 1.0 Define a function that takes arguments.
-# 2.0 Return a value from a function.
-# 3.0 Test a function.
-# 4.0 Set default values for function arguments.
-# 5.0 Explain why we should divide programs into small, single-purpose functions.
+# 
+# 1.0 Multiply the fractional numbers by 100.
+# 
+# 2.0 Round the result to one decimal place.
+# 
+# 3.0 You can use the round() function to do this.
+# 
+# 4.0 Paste a percentage sign after the rounded number.
+# 
+# 5.0 The paste() function is at your service to fulfill this task.
+# 
+# 6.0 Print the result.
+# 
+# The print() function will do this.
 
 
 
